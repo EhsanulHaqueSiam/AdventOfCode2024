bb21ac5b9afa825d40218aaf70c54c1c9cf6827f
EhsanulHaqueSiam <ehsanul.siamdev@gmail.com>
2024-12-13 12:56:31 +0600

    Initial commit: Advent of Code 2024 setup

---
+++ day01/Day01Part1_V1.kt
@@ -0,0 +1,25 @@
+
+import java.io.File
+import kotlin.math.abs
+
+fun readInput(filename: String): List<String> = File(filename).readLines()
+
+fun main() {
+    val lines = readInput("day01.txt")
+    val (left, right) =
+        lines
+            .map { line ->
+                val first = line.substringBefore(" ").toInt()
+                val second = line.substringAfterLast(" ").toInt()
+                first to second
+            }.unzip()
+    val result =
+        left
+            .sorted()
+            .zip(right.sorted())
+            .map { (first, second) ->
+                abs(first - second)
+            }.sum()
+
+    println("Result: $result")
+}
